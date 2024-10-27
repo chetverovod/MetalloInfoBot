b06b3d95014dad653df0f04f7d2f755b82aa7ee7
Igor Plastov <chetverovod@gmail.com>
2024-10-27 15:33:28 +0300

    DEbug.

--- solver.py
+++ solver.py
@@ -16,19 +16,7 @@ answer_1 = 'Временное сопротивление, предел теку
            ' ударная вязкость -40 (KCU), ударная вязкость (KCU) мех. старение' \
            ' при комнатной температуре.'
 
-"""
-Вопросы к ИИ
-1. Какой тип проката в запросе?
-1. Какое исполнение?
-1. Марка стали?
-1. Толщина?
-1. Класс прочности?
-1. Категория?
-1. Номер ГОСТа? 
-1. Какие таблицы ГОСТа относятся к (тип + исп) прокату?
-1. Среди выбранных таблиц какие упоминают такие класс прочности и марку стали?
-1. Какие сведения соответствуют толщине  и категории?
-"""
+
 book = []
 system_msg = {
                 'role': 'system',
@@ -43,7 +31,7 @@ book.append(system_msg)
 models_config_file = 'models.cfg'
 
 
-def rag(context, gost_num: str = "", gost_year: str = ""):
+def rag(context, gost_num: str = "", gost_year: str = "", show=False):
     collection = mio.get_collection('metalloprokat')
     n_res = 5
     sz = 0
@@ -60,29 +48,122 @@ def rag(context, gost_num: str = "", gost_year: str = ""):
         n_res += 1
     print(f"RAG volume = {sz}")
     print(f"RAG list len = {len(rag_context)}")
+    if show:
+        print(colored(f'rag docs<{rag_context}>', 'green'))
     return rag_context
 
 
-def ai(prompt_txt: str) -> str:
-    opt = {"temperature": 0, "num_ctx": NUM_CTX}  
+def ai(prompt_txt: str, show=False) -> str:
+    if show:
+        print(colored(f"promt: {prompt_txt}", 'light_yellow'))
+    opt = {"temperature": 0, "num_ctx": NUM_CTX}
     stream = ollama.generate(model='llama3.1', prompt=prompt_txt, options=opt)
-    res = stream["response"]
+    res = stream["response"].removesuffix('.')
     print(colored(f'>>> {res}\n', 'yellow'))
     return res
 
-
+"""
+Вопросы к ИИ
+1. Какой тип проката в запросе?
+1. Какое исполнение?
+1. Марка стали?
+1. Толщина?
+1. Класс прочности?
+1. Категория?
+1. Номер ГОСТа? 
+1. Какие таблицы ГОСТа относятся к (тип + исп) прокату?
+1. Среди выбранных таблиц какие упоминают такие класс прочности и марку стали?
+1. Какие сведения соответствуют толщине  и категории?
+"""
 print(colored("Разбор запроса", "red"))
+
+qwe = {}
+
+# Получаем тип проката
+prompt = f"Какой тип проката упомянут в тексте: {query_1} Ответь двумя словами."
+qwe['prokat_target'] = ai(prompt)
+print('prokat_target:', qwe['prokat_target'])
+
+# Получаем название ГОСТа
 prompt = f"Изучи текст: {query_1} Какой " \
          " ГОСТ в нем упомянут? Выведи только обозначение ГОСТа."
-
 res = ai(prompt)
-
 prompt = f"Извлеки из текста обозначение ГОСТа. Вот из этого текста: {res}"
 gost = ai(prompt)
-gost_num = gost.split('-')[0]
-print('Num:', gost_num)
-gost_year = gost.split('-')[1]
-print('Year:', gost_year)
+qwe['gost_num'] = gost.split('-')[0]
+print('GOST Num:', qwe['gost_num'])
+qwe['gost_year'] = gost.split('-')[1]
+print('GOST Year:', qwe['gost_year'])
+
+# Получаем исполнение проката
+prompt = f'Какое исполнение упомянуто в тексте: "{query_1}"' \
+          '  Ответь одним словом.'
+
+qwe['prokat_option'] = ai(prompt, show=True)
+print('prokat_option:', qwe['prokat_option'])
+
+# Проверяем, что в ГОСТе упоминается это исполнение проката
+docs = rag(f'исполнение, исполнения', qwe['gost_num'], qwe['gost_year'],
+           show=True)
+opt = qwe['prokat_option']
+prompt = f'В этом тексте: "{docs}", встречается исполнение "{opt}"? Ответь коротко.'
+prokat_option_in_gost = ai(prompt, show=True)
+if 'да' in  prokat_option_in_gost.lower():
+    qwe['option_in_gost'] = True
+else:
+    qwe['option_in_gost'] = False
+
+# Получаем марку стали
+prompt = f'Какая марка стали упомянута в тексте: "{query_1}"' \
+          '  Ответь одним словом.'
+qwe['steel'] = ai(prompt, show=True)
+
+# Проверяем, что в ГОСТе упоминается эта марка стали
+docs = rag(f'сталь', qwe['gost_num'], qwe['gost_year'],
+           show=True)
+opt = qwe['steel']
+prompt = f'В этом тексте: "{docs}", встречается марка стали "{opt}"? Ответь коротко.'
+steel_mark_in_gost = ai(prompt, show=True)
+if 'да' in steel_mark_in_gost.lower():
+    qwe['steel_in_gost'] = True
+else:
+    qwe['steel_in_gost'] = False
+
+# Получаем толщину проката
+prompt = f'Какая толщина упомянута в тексте: "{query_1}"' \
+          '  Ответь одним словом.'
+qwe['thickness'] = ai(prompt, show=True)
+
+# Проверяем, что в ГОСТе упоминается эта толщина проката
+docs = rag(f'сталь', qwe['gost_num'], qwe['gost_year'],
+           show=True)
+opt = qwe['thickness']
+prompt = f'В этом тексте: "{docs}", встречается толщина проката "{opt}"? Ответь коротко.'
+steel_mark_in_gost = ai(prompt, show=True)
+if 'да' in steel_mark_in_gost.lower():
+    qwe['thickness_in_gost'] = True
+else:
+    qwe['thickness_in_gost'] = False
+
+# Получаем класс прочности проката
+prompt = f'Какой класс прочности упомянут в тексте: "{query_1}"' \
+          '  Ответь одним словом.'
+qwe['solidity'] = ai(prompt, show=True)
+
+# Проверяем, что в ГОСТе упоминается эта марка стали
+docs = rag(f'класс прочности', qwe['gost_num'], qwe['gost_year'],
+           show=True)
+opt = qwe['solidity']
+prompt = f'В этом тексте: "{docs}", встречается класс прочности "{opt}"? Ответь коротко.'
+steel_mark_in_gost = ai(prompt, show=True)
+if 'да' in steel_mark_in_gost.lower():
+    qwe['solidity_in_gost'] = True
+else:
+    qwe['solidity_in_gost'] = False
+
+
+print(qwe)
+exit(0)
 
 
 full = False
@@ -91,8 +172,7 @@ if full:
     gost = 'ГОСТ 19281-2014'
     gost_num = '19281'
     gost_year = '2014'
-    docs = rag('стандарт распространяется', gost_num, gost_year)
-    print(colored(f'docs<{docs}>', 'green'))
+    docs = rag('стандарт распространяется', gost_num, gost_year, show=True)
     prompt = "Извлеки из текста информацию о том," \
              f" на какой горячекатанный прокат распространяется настоящий стандарт и перечисли виды этого проката." \
              f" Вот из этого текста: {docs}. Отвечай на русском языке."
@@ -101,8 +181,7 @@ if full:
     prompt = f"Какой тип проката упомянут в тексте: {query_1} Ответь двумя словами."
     prokat_target = ai(prompt)
 
-    docs = rag(f'испытан', gost_num, gost_year)
-    print(colored(f'docs<{docs}>', 'green'))
+    docs = rag(f'испытан', gost_num, gost_year, show=True)
     prompt = f"Каким испытания должен выдержать прокат {prokat_target}? Найди ответ в тексте: " \
              f"{docs}. Отвечай на русском языке."
     prokat_tests = ai(prompt)
